ec5920eaecf6569a0e80cc965fbaf83b23cdd3be
unknown <kenblackuoo@gmail.com>
2022-02-15 17:03:30 -0500

    Updated the models.py and serializers.py files to support the BONUS user story of adding an image_link to each product + Updated Postman Collection file

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 #(2.5 points) As a developer, I want to create a Product model. Property names must be in snake_case and match the following exactly!
 # title - CharField | description - CharField | price - DecimalField | inventory_quantity - IntegerField
-    # lines 8 - 12 satisfy this user story
+    # lines 10 - 14 satisfy this user story
+# BONUS: (5 points) As a developer, I want to add the ability to add an image link to each product. (Link to picture on the internet)
+    # line 15 for the image link
 
 from django.db import models
 
@@ -10,5 +12,6 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image_link = models.URLField(max_length=2500) #this variable is for the bonus user story
     
      
--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,10 @@
+# BONUS: (5 points) As a developer, I want to add the ability to add an image link to each product. (Link to picture on the internet)
+    # line 10 contains 'image_link' to allow users to make requests (e.g. PUT) from this table column
+
 from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'price', 'inventory_quantity'] #these are from the table columns
+        fields = ['id', 'title', 'description', 'price', 'inventory_quantity', 'image_link'] #these correspond to the table's columns
